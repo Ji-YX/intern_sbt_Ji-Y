e9e045aa19c2b29f3e83a8262165815c809e12ed
Ji-YX <124946520+Ji-YX@users.noreply.github.com>
2023-04-05 20:09:57 +0900

    Changed the arguments of mintAndTransfer in SharedNFT-test.js

--- subfolder_tmp/tests/scripts/SharedNFT-test.js
+++ subfolder_tmp/tests/scripts/SharedNFT-test.js
@@ -14,7 +14,6 @@ const main = async () => {
     [0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199, 0xdD2FD4581271e230360230F9337D5c0430Bf44C0],
     [],
     [],
-    accounts[0]
   );
   await txn.wait();
 
